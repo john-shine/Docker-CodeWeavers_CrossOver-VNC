13b6ccf6e9b5ecc34254a5365c08ebf4717eb459
john-shine <mr.john.shine@gmail.com>
2019-10-18 22:34:17 -0700

    fix Abnormal exit

--- BaiduNetdisk/entrypoint.sh
+++ BaiduNetdisk/entrypoint.sh
@@ -4,5 +4,5 @@ if [[ -f ${HOME}/.vnc/passwd ]]; then
     echo "${vnc_password}" | vncpasswd -f > ${HOME}/.vnc/passwd
 fi
 
-[ -z "${DISPLAY}" ] || /usr/bin/vncserver -kill ${DISPLAY}; sudo rm -f /tmp/.X*-lock; sudo rm -rf /tmp/.X11-unix/; sleep 2
+[ -z "${DISPLAY}" ] || /usr/bin/vncserver -kill ${DISPLAY}; sudo rm -f /tmp/.X*-lock; sudo rm -rf /tmp/.X11-unix/*; sleep 2
 /usr/bin/vncserver -geometry 1024x800 -fg
