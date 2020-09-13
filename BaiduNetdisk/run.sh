c345d7e5339f04a4a733adc94648117a7fa63e4c
nas <nas@glan.space>
2020-09-13 12:00:43 +0800

    client version fixed on 3.0.1.2

--- BaiduNetdisk/run.sh
+++ BaiduNetdisk/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version=$(cat $(pwd "$0")/VERSION)
-sudo docker run -d -p 6080:6080 -p 5901:5901 johnshine/baidunetdisk-crossover-vnc:${version}
+sudo docker run -d --add-host update.pan.baidu.com:127.0.0.1 -p 6080:6080 -p 5901:5901 johnshine/baidunetdisk-crossover-vnc:${version}
 
 # The following is for test
 # running=$(sudo docker ps -q --filter status=running)
